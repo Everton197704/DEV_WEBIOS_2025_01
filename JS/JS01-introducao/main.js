6038e2972b83d33f1939aebdd4fd56a6ed511bcf
Everton197704 <everton2015santana@gmail.com>
2025-06-03 21:39:42 -0300

    Aula 01 introdução JS

---
+++ JS/JS01-introducao/main.js
@@ -0,0 +1,27 @@
+var idade
+// javascript
+console.log("Hello world") //Apresentar uma mensagem
+
+console.clear() //limpar a tela
+// Variaveis
+
+var numero1 = 192 //inicializar uma variavel
+console.log(numero1)
+
+var num1 = 4
+var num2 = 4
+
+var resultado = num1 + num2
+console.log (num1 + num2)
+
+idade = 89 // inicia a variavel em outra parte do codigo
+console.log (idade)
+
+// textos - strings
+var nome ='Matheus Araujo'
+var nome2 ='Matheus vinicius'
+var nome3 ='221321321' //texto
+
+console.log (nome) 
+console.log (nome2)
+console.log (nome3)
