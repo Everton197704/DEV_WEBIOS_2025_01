f8b9842945ced1b0d321ad1be9e996455e8ee81a
Everton197704 <everton2015santana@gmail.com>
2025-06-15 22:10:37 -0300

    ExercicioCasa04

---
+++ JS/JS03-Estrutura _Condionais/JS03-ExercicioCasa/main.js
@@ -0,0 +1,18 @@
+let nome = 'Everton Santana'
+
+let notaProva1 = 4;
+let notaProva2 = 3;
+let notaProva3 = 7;
+let notaProva4 = 5;
+
+let media = (notaProva1+notaProva2+notaProva3+notaProva4) /4
+
+console.log("A MÉDIA DA ALUNA " + nome + " COM ÁS 4 NOTAS É " + media);
+
+if (media >=7){
+console.log(" APROVADA ");
+}
+
+else{
+console.log(" REPROVADO ");
+}
